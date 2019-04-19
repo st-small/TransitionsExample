ef6a0aa917c092fe5160dd097a14892de5e9f9fa
Станислав Шияновский <st-small@rambler.ru>
2019-04-19 20:52:30 +0300

    Initial Commit

---
+++ TransitionsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransitionsExample
+//
+//  Created by Stanly Shiyanovskiy on 4/19/19.
+//  Copyright © 2019 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
